fce309a191b1e6f34e473b76c758d08a2c756d24
Sergey <10rubleqow@gmail.com>
2019-10-09 18:07:03 +0300

    Первая защита!

---
+++ js/favorites_basket-min.js
@@ -0,0 +1,2 @@
+var basket=document.querySelector(".basket");var favorites=document.querySelector(".favorites");var addBasket=document.querySelectorAll(".buy-btn");var addFavorites=document.querySelectorAll(".to-fav-btn");for(var i=0;i<addBasket.length;i++){addBasket[i].addEventListener("click",function(evt){evt.preventDefault();basket.classList.add("basket-catalog")})}
+for(var i=0;i<addFavorites.length;i++){addFavorites[i].addEventListener("click",function(evt){evt.preventDefault();favorites.classList.add("favorites-catalog")})}
